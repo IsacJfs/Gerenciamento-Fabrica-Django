6f7cbb87619098bcdb04dd76331f38c4a08d77e0
IsacJfs <isac.jfs@gmail.com>
2023-08-24 13:52:48 -0300

    :construction: Criados tabelas de cadastro adicionando conf de segurança

--- fabrica/fabrica/settings.py
+++ fabrica/fabrica/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mq054n*@hsqc7x$f+b66hrv(b*7ayc*b$h)g!b@3h@ufbi^^ir'
+SECRET_KEY = 'q6IaMQ30NphSlZmaKNn3vwZIPOFBQhy43JJNY8llyUUhNiR249uKwtivr13eDo7fz2xf4DVmjAiT451vNrVsDKv7zJHQIepmMgj0TXglX0gNqKGRfV46JkFvWxCEJqf0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://gerenciamento-template.vercel.app/']
 
 
 # Application definition
@@ -132,6 +132,3 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 
 CSRF_COOKIE_SECURE = True
-
-DEBUG = False
-
